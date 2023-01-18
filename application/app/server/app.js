ad63873f4a530c6e68b8207e0f2dee2985b6d5b9
Etudiant O'clock <vlad.cirjan09@gmail.com>
2023-01-18 19:51:25 +0100

    Ajout d'un favicon / Fichier index dans routes renommer en mainRouter / Remodelisation du server app.js / Demarrage de l'EJS

--- application/app/server/app.js
+++ application/app/server/app.js
@@ -1,20 +1,20 @@
+//Ajouter les variables d'environnement custom (renseignée dans un fichier .env) dans l'objet `process.env`.
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const router = require("../routes/mainRouter");
 const PORT = 5000;
-
-//Importer le module dotenv
-const dotenv = require("dotenv");
-//Ajouter les variables d'environnement custom (renseignée dans un fichier .env) dans l'objet `process.env`.
-dotenv.config();
-
-
 
 //Cherche si la requête correspond à un fichier statique existant dans le répertoire spécifié.
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", "app/views");
+
+app.use(router);
+
 app.listen(PORT, () => {
     console.log(`Listening on http://vlad-cirjan.vpnuser.lan:${PORT}/`);
 });
-
-
-module.exports = app;
