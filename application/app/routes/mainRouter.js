d1ffc3f7f42db1a1497c2503bc05b726c07086fa
Etudiant O'clock <vlad.cirjan09@gmail.com>
2023-01-18 20:24:58 +0100

    Réalisation de l'EJS / Connexion de l'EJS via les routes

--- application/app/routes/mainRouter.js
+++ application/app/routes/mainRouter.js
@@ -5,8 +5,29 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-	res.render("index");
+//Route de la page d'accueil
+router.get("/homepage", (req, res) => {
+	res.render("homepage");
+});
+
+//Route de la page aide
+router.get("/aide", (req, res) => {
+	res.render("aide");
+});
+
+//Route de la page contact
+router.get("/contact", (req, res) => {
+	res.render("contact");
+});
+
+//Route de la page des droits politiques des cookies
+router.get("/cookies", (req, res) => {
+	res.render("cookies");
+});
+
+//Route de la page d'une liste
+router.get("/liste", (req, res) => {
+	res.render("liste");
 });
 
 module.exports = router;
